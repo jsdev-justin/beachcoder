7bd6a4e7b4ec06adc9cb1d23fe2c3564f1882232
Justin Green <JGreen72181@hotmail.com>
2021-04-28 18:12:12 -0700

    push

---
+++ script.js
@@ -0,0 +1,23 @@
+
+var html=""
+var linkCol = document.querySelector(".link-col");
+var burger = document.querySelector(".burger");
+var menu = document.querySelector(".menu");
+
+testimonials.forEach(t=>{
+    html += `<div class='white p-2'><p class='p'>${t.comment}</p><h5>-${t.name}</h5><p class='p'>Rated:<span class='yellow margin-left'>${t.rated}</span></p></div>`
+})
+
+linkCol.innerHTML = html;
+
+
+burger.onclick=()=>{
+    console.log('fx fired')
+    // menu.classList.toggle("active");
+    // var content = this.nextElementSibling;
+    if (menu.style.maxHeight){
+      menu.style.maxHeight = null;
+    } else {
+      menu.style.maxHeight = '200px';
+    }
+}
---
+++ testimonials.js
@@ -0,0 +1,6 @@
+const testimonials=[
+    {id:1,name:"javascriptnewb",comment:"Beach Coders gave me the start I need to build stuff I never thought possible!",rated:'5'},
+    {id:2,name:"formreqstsue",comment:"Ive tried coding sites, youtube videos, it wasnt until beach coders that things clicked() and got me to where I'm now a working professional!",rated:'4'},
+    {id:3,name:"helloworldlarry",comment:"Beach Coders is the best. Loved it!",rated:'5'},
+    {id:4,name:"gitprogramming",comment:"I can't believe I almost didn't take this class, man how life has improved since I did!",rated:'4.5'},
+]
